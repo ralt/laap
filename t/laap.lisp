2f09162748401b9cb29b7558249d77393ce8c355
Florian Margaine <florian@platform.sh>
2017-04-30 01:28:46 +0200

    Officialize (laap-test::main)

--- t/laap.lisp
+++ t/laap.lisp
@@ -3,6 +3,13 @@
 (def-suite laap :description "Baz")
 (in-suite laap)
 
+;;; Run with: /buildapp --output bin/foo --asdf-tree ~/quicklisp/ --load-system laap-test --entry laap-test::main --eval '(setf *debugger-hook* (lambda (c h) (declare (ignore h)) (format t "~A~%" c)))'
+;;; for strace runs et al.
+(defun main (&rest args)
+  (laap:with-magic
+    (loop for i below 3
+       do (laap:spawn (wait-and-print i)))))
+
 (test delay
   (laap:with-magic
     (format t "foo~%")
