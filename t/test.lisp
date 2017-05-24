d8f7ec8aaae2a4e4b764e75f6e3e3502f240ca13
Florian Margaine <florian@platform.sh>
2017-05-25 01:58:28 +0200

    Truly asynchronous tests

--- t/test.lisp
+++ t/test.lisp
@@ -5,22 +5,34 @@
 (defmacro test (name var &body body)
   `(setf (gethash ',name *tests*) (lambda ,var ,@body)))
 
-(defvar *results* nil)
-
 (defun run-all-tests ()
-  (maphash #'run *tests*))
+  (let ((end (bt:make-condition-variable))
+	(end-lock (bt:make-lock))
+	(counter (hash-table-count *tests*)))
+    (maphash (lambda (test-name test-callback)
+	       (bt:make-thread
+		(lambda ()
+		  (bt:with-lock-held (end-lock)
+		    (run test-name test-callback
+			 (lambda ()
+			   (decf counter)
+			   (when (= counter 0)
+			     (bt:condition-notify end))))))))
+	     *tests*)
+    (bt:with-lock-held (end-lock)
+      (bt:condition-wait end end-lock))))
 
 (defun p (&rest args)
   (apply #'format t args)
   (force-output))
 
-(defun run (test-name test-callback)
-  (let ((result nil))
+(defun run (test-name test-callback c)
+  (flet ((done (&optional (err t))
+	   (if (eq err t)
+	       (p "passed.~%" test-name)
+	       (p "failed with: ~a~%" test-name err))
+	   (funcall c)))
     (p "Testing ~a... " test-name)
     (laap:with-event-loop
-      (laap:add-reporter (lambda (err)
-			   (setf result err)))
-      (funcall test-callback (lambda ())))
-    (if result
-	(p "failed with: ~a~%" test-name result)
-	(p "passed.~%" test-name))))
+      (laap:add-reporter #'done)
+      (funcall test-callback #'done))))
