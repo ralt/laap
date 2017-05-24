0aa2cb6d40d91a9115e38338fdd1694177e02a2c
Florian Margaine <florian@platform.sh>
2017-05-25 01:25:32 +0200

    Cosmetic intervention

--- t/test.lisp
+++ t/test.lisp
@@ -10,13 +10,17 @@
 (defun run-all-tests ()
   (maphash #'run *tests*))
 
+(defun p (&rest args)
+  (apply #'format t args)
+  (force-output))
+
 (defun run (test-name test-callback)
   (let ((result nil))
-    (format t "Testing ~a... " test-name)
+    (p "Testing ~a... " test-name)
     (laap:with-event-loop
       (laap:add-reporter (lambda (err)
 			   (setf result err)))
       (funcall test-callback (lambda ())))
     (if result
-	(format t "failed with: ~a~%" test-name result)
-	(format t "passed.~%" test-name))))
+	(p "failed with: ~a~%" test-name result)
+	(p "passed.~%" test-name))))
