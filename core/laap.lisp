1102f824ec9f126968878e2f473bbd07bc815210
Florian Margaine <florian@platform.sh>
2017-05-25 01:35:02 +0200

    Fix startup by blocking threads.

--- core/laap.lisp
+++ core/laap.lisp
@@ -4,21 +4,30 @@
 (defvar *loop* nil)
 
 (defmacro with-event-loop (&body body)
-  `(let* ((*thread-pool* (make-instance 'thread-pool))
-	  (*loop* (make-instance 'event-loop))
-	  (bt:*default-special-bindings* `((*thread-pool* . ,*thread-pool*)
-					   (*loop* . ,*loop*))))
-     (add-reporter (lambda (err)
-		     (format t "A thread died with error: ~a~%" err)))
-     (unwind-protect
-	  (progn ,@body)
-       (let ((thread-pool-thread (start-thread-pool)))
-	 (start-event-loops)
-	 (bt:join-thread thread-pool-thread)
-	 (maphash (lambda (thread props)
-		    (declare (ignore props))
-		    (bt:join-thread thread))
-		  (threads *thread-pool*))))))
+  (let ((err (gensym))
+	(res (gensym)))
+    `(let* ((*thread-pool* (make-instance 'thread-pool))
+	    (*loop* (make-instance 'event-loop))
+	    (bt:*default-special-bindings* `((*thread-pool* . ,*thread-pool*)
+					     (*loop* . ,*loop*))))
+       (add-reporter (lambda (err)
+		       (format t "A thread died with error: ~a~%" err)))
+       (unwind-protect
+	    ;; We're immediately adding to the event loop,
+	    ;; so that if it starts with a blocking thread,
+	    ;; it will first go through the event loop, making
+	    ;; sure both the thread pool and the event loop
+	    ;; threads are started.
+	    (spawn (lambda (,err ,res)
+		     (declare (ignore ,err ,res))
+		     ,@body))
+	 (let ((thread-pool-thread (start-thread-pool)))
+	   (start-event-loops)
+	   (bt:join-thread thread-pool-thread)
+	   (maphash (lambda (thread props)
+		      (declare (ignore props))
+		      (bt:join-thread thread))
+		    (threads *thread-pool*)))))))
 
 (defun noop (err res)
   (declare (ignore err res)))
